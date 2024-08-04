157291c28da9decf6267e4e022a4144171a76ec2
Mlie <emipa606@users.noreply.github.com>
2024-08-04 18:38:21 +0200

    Found a better solution for the priority issues, also fixes interaction with Project RimFactory Revived

---
+++ Source/FlickableStorage/StoreUtility_TryFindBestBetterStoreCellForWorker.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using RimWorld;
+
+namespace FlickableStorage;
+
+[HarmonyPatch(typeof(StoreUtility), "TryFindBestBetterStoreCellForWorker")]
+internal class StoreUtility_TryFindBestBetterStoreCellForWorker
+{
+    private static bool Prefix(ISlotGroup slotGroup)
+    {
+        if (slotGroup.Settings.owner is not IHaulDestination destination)
+        {
+            return true;
+        }
+
+        var storageTracker = destination.Map.GetStorageTracker();
+        if (storageTracker == null)
+        {
+            return true;
+        }
+
+        return !storageTracker.Has(destination) ||
+               storageTracker[destination] == 0 || storageTracker[destination] == 2;
+    }
+}
